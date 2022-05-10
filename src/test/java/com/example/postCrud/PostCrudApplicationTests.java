aad22896dcadae337ad8acd4236dbe705c11f8cc
meme7 <manlopez77@gmail.com>
2022-05-09 19:46:43 -0600

    setup

---
+++ src/test/java/com/example/postCrud/PostCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
